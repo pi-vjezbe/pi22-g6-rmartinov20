d7f946358e580cb24f11e32b34df8030c25dd21d
Programsko Inzenjerstvo <pi_foi@outlook.com>
2022-05-09 19:54:01 +0200

    Sve promijene za evaluaciju

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Evaluation_Manager
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.cboxActivities = new System.Windows.Forms.ComboBox();
+			this.txtMinForSignature = new System.Windows.Forms.TextBox();
+			this.txtMinForGrade = new System.Windows.Forms.TextBox();
+			this.txtActivityDescription = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.txtTeacher = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.numPoints = new System.Windows.Forms.NumericUpDown();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numPoints)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(369, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Uvjet za potpis";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(591, 31);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(81, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Uvjet za ocjenu";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(51, 30);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Aktivnost";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// cboxActivities
+			// 
+			this.cboxActivities.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboxActivities.FormattingEnabled = true;
+			this.cboxActivities.Location = new System.Drawing.Point(54, 60);
+			this.cboxActivities.Name = "cboxActivities";
+			this.cboxActivities.Size = new System.Drawing.Size(184, 21);
+			this.cboxActivities.TabIndex = 3;
+			// 
+			// txtMinForSignature
+			// 
+			this.txtMinForSignature.Location = new System.Drawing.Point(356, 61);
+			this.txtMinForSignature.Name = "txtMinForSignature";
+			this.txtMinForSignature.ReadOnly = true;
+			this.txtMinForSignature.Size = new System.Drawing.Size(100, 20);
+			this.txtMinForSignature.TabIndex = 4;
+			// 
+			// txtMinForGrade
+			// 
+			this.txtMinForGrade.Location = new System.Drawing.Point(594, 61);
+			this.txtMinForGrade.Name = "txtMinForGrade";
+			this.txtMinForGrade.ReadOnly = true;
+			this.txtMinForGrade.Size = new System.Drawing.Size(100, 20);
+			this.txtMinForGrade.TabIndex = 5;
+			// 
+			// txtActivityDescription
+			// 
+			this.txtActivityDescription.Location = new System.Drawing.Point(64, 111);
+			this.txtActivityDescription.Multiline = true;
+			this.txtActivityDescription.Name = "txtActivityDescription";
+			this.txtActivityDescription.Size = new System.Drawing.Size(659, 115);
+			this.txtActivityDescription.TabIndex = 6;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(358, 251);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(57, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Ocjenjivač";
+			this.label4.Click += new System.EventHandler(this.label4_Click);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(459, 251);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(89, 13);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Datum evaluacije";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(630, 251);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(40, 13);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "Bodovi";
+			this.label6.Click += new System.EventHandler(this.label6_Click);
+			// 
+			// txtTeacher
+			// 
+			this.txtTeacher.Location = new System.Drawing.Point(335, 278);
+			this.txtTeacher.Name = "txtTeacher";
+			this.txtTeacher.ReadOnly = true;
+			this.txtTeacher.Size = new System.Drawing.Size(100, 20);
+			this.txtTeacher.TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(472, 278);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 12;
+			// 
+			// numPoints
+			// 
+			this.numPoints.Location = new System.Drawing.Point(601, 279);
+			this.numPoints.Name = "numPoints";
+			this.numPoints.Size = new System.Drawing.Size(120, 20);
+			this.numPoints.TabIndex = 14;
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(548, 343);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 15;
+			this.btnSave.Text = "Spremi";
+			this.btnSave.UseVisualStyleBackColor = true;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(646, 343);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 16;
+			this.btnCancel.Text = "Odustani";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.numPoints);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.txtTeacher);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.txtActivityDescription);
+			this.Controls.Add(this.txtMinForGrade);
+			this.Controls.Add(this.txtMinForSignature);
+			this.Controls.Add(this.cboxActivities);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numPoints)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox cboxActivities;
+		private System.Windows.Forms.TextBox txtMinForSignature;
+		private System.Windows.Forms.TextBox txtMinForGrade;
+		private System.Windows.Forms.TextBox txtActivityDescription;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox txtTeacher;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.NumericUpDown numPoints;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnCancel;
+	}
+}
